dd7660a66b55d5d53900436cd57ee8ba75ec49f6
David Li <63334933+9t8@users.noreply.github.com>
2026-02-03 13:49:51 -0500

    Simplify next.config.js by removing webpack config
    Removed custom webpack configuration for externals.

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  webpack: (config) => {
-    config.externals.push({
-      sharp: "commonjs sharp",
-    });
-
-    return config;
-  },
 };
 
 module.exports = nextConfig;
